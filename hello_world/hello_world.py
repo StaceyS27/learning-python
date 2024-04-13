9d11fdb2238cb506e6938b9cdf74d387144008d1
Stacey Sanchez <staceysanchez27@gmail.com>
2024-04-13 00:17:09 -0400

    variables and printing in python

---
+++ hello_world/hello_world.py
@@ -0,0 +1,44 @@
+# comments are written this way 
+
+# Print
+print("hello world")  # what is used instead of console.log()
+
+
+# Strings - can use single and double quotes like javascript 
+print('hello world')
+
+
+# Variables 
+    # can't have spaces or symbols other than underscore ( _ )
+    # can't start w/ numbers but can have them after first letter 
+
+message = "hasta la vista"
+print(message)
+
+#_______________________________________________________________________________________
+# Errors
+    # two common types of errors occur while writing python: SyntaxError and NameError
+    # 1. SyntaxError - something is wrong with usage of language 
+        # use of punctuation, paranthesis, command etc
+    # 2. NameError - occurs when python interpreter does not recognize a word 
+        # a variable that is never defined will throw this error 
+
+#_______________________________________________________________________________________
+# Numbers
+    # python has a few numberic data types 
+        # 1. integer - (int) is a whole number ex: 1,2,3 also -1,-2,-2 and 0
+        # 2. floating-point number - (float) is a decimal
+
+# Define the release and runtime integer variables below:
+release_year = 1
+runtime = 10
+
+# Define the rating_out_of_10 float variable below: 
+rating_out_of_10 = 2.5
+print(rating_out_of_10)
+
+#_______________________________________________________________________________________
+# Calculations
+
+
+
