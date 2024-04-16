442418db0d32f623fc4cd9419e5f8eb2e2faf422
Stacey Sanchez <staceysanchez27@gmail.com>
2024-04-16 12:56:17 -0400

    ASCII Art using multi-line strings and print statements

---
+++ hello_world/block_letters.py
@@ -0,0 +1,34 @@
+# Stacey Sanchez
+# I have a twin sister!
+
+# ASCII Art
+
+initials = """
+ sss           sss
+s   s         s   s
+s             s
+ sss           sss
+    s             s
+s   s         s   s
+ sss           sss
+"""
+print(initials)
+
+print("  SSS  ")
+print(" S   S ")
+print(" S     ")
+print("  SSS  ")
+print("     S ")
+print(" S   S ")
+print("  SSS  ")
+
+print(" ")
+
+print("  SSS  ")
+print(" S   S ")
+print(" S     ")
+print("  SSS  ")
+print("     S ")
+print(" S   S ")
+print("  SSS  ")
+
