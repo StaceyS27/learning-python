b493ab4876487a842b408174504497266e977900
Stacey Sanchez <staceysanchez27@gmail.com>
2024-04-18 12:15:26 -0400

    input function in pythin

---
+++ hello_world/user_input.py
@@ -0,0 +1,10 @@
+# User Input
+
+# it is how to assign a user's input as a variable
+# uses input function, which requires a prompt message 
+    # the prompt messge is printed for the user
+    # before they enter the new information 
+
+likes_snakes = input("Do you like snakes?")
+# likes_snakes will be assigned a value of the user's answer
+# program will print "Do you like snakes?" which is the prompt the user will answer
