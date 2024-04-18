958d2f53b250fd49b435a686838913ea0329e922
Stacey Sanchez <staceysanchez27@gmail.com>
2024-04-18 12:54:59 -0400

    declaring and checking type of boolean variables

--- control_flow/control_flow.py
+++ control_flow/control_flow.py
@@ -11,3 +11,25 @@ statement_two = False
 #____________________________________________________________________________
 # Boolean Variables 
 
+# True and False are its own data type - bool
+
+my_baby_bool = "true"
+print(type(my_baby_bool))   
+# prints <class 'str'>
+
+my_baby_bool_two = True
+print(type(my_baby_bool_two))
+# prints <class 'bool'>
+
+# boolean variables can be greated by ..
+# 1. setting variable to true or false  or 
+set_to_true = True
+set_to_false = False
+
+# 2. set a variable to equal a boolean expression 
+bool_three = 3 * 3 == 9
+print(bool_three)  # bool_three variable will now be True and will print that
+
+#_______________________________________________________________________________
+# If Statement 
+
