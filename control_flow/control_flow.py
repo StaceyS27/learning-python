5a411f1cfc98d8cddf24127923671cb915678660
Stacey Sanchez <staceysanchez27@gmail.com>
2024-04-18 12:36:59 -0400

    relational operators in python

---
+++ control_flow/control_flow.py
@@ -0,0 +1,13 @@
+# Relational Operators: Equals and Not Equals
+
+# comparators
+    # equals: ==
+    # not equals: !=
+
+# True and False - first letter capitalized 
+statement_one = True
+statement_two = False 
+
+#____________________________________________________________________________
+# Boolean Variables 
+
