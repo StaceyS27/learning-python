10979e64906dc760c7fef331dc2061842f559913
Stacey Sanchez <staceysanchez27@gmail.com>
2024-04-26 23:12:04 -0400

    error types in python

---
+++ control_flow/errors.py
@@ -0,0 +1,17 @@
+# Syntax Errors 
+# means there is something wrong with the way your program is written
+# usually arrises from:
+    # misspelling python keyword
+    # missing colon : 
+    # missing paranthesis, square bracket, or curly brace 
+
+# name errors 
+# when it detects variable that is unknown
+# misspelled variable
+# forgetting to define a variable 
+
+# type errors
+# thrown when an operation is applied to a variable of an inappropriate type 
+
+piggy_bank = '2' + 0.25
+# would get a type error saying must be a str, not float 
