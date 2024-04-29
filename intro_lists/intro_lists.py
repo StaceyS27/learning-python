267b42f1f54a68184e005e5c4bb6d4dcd4845513
Stacey Sanchez <staceysanchez27@gmail.com>
2024-04-29 00:31:07 -0400

    intro to list methods

--- intro_lists/intro_lists.py
+++ intro_lists/intro_lists.py
@@ -14,3 +14,24 @@ sam_height_and_testscore = ["Sam", 67, 85.5, True]
 
 #_________________________________________________________________________________
 # Empty Lists
+my_empty_list = []
+
+#_________________________________________________________________________________
+# List Methods
+
+# append() adds element at the end of the list 
+# will explore more methods 
+
+example_list = [1, 2, 3, 4]
+
+#Using Append
+example_list.append(5)          # like push in js
+# print(example_list)
+print(example_list)             # [1, 2, 3, 4, 5]
+
+#Using Remove
+example_list.remove(5)          # like pop in js
+# print(example_list)
+print(example_list)             # [1, 2, 3, 4]
+
+#_________________________________________________________________________________
