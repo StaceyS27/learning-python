cf9a885304bf34ecc4e51d51cfea4d711c61fe5a
Stacey Sanchez <staceysanchez27@gmail.com>
2024-04-29 00:18:21 -0400

    different data types in a list

--- intro_lists/intro_lists.py
+++ intro_lists/intro_lists.py
@@ -1,7 +1,16 @@
 # What is a List?
 
 # a list is one of many built-in data structures that allow us to work with 
-# a collection of data in sequential order 
+# a collection of data in sequential order (meaning contigous in memory)
 heights = [61, 70, 67, 64, 65]
 
 #________________________________________________________________________________
+# What can a List contain?
+
+# lists can contain, integers, strings and even multiple data types in one list 
+# below it has a string, integer, boolean, and float datatype 
+mixed_list_common = ["Mia", 27, False, 0.5]
+sam_height_and_testscore = ["Sam", 67, 85.5, True]
+
+#_________________________________________________________________________________
+# Empty Lists
