366f9191b4538dae1634c04ec7062dc4a4f11a2b
Stacey Sanchez <staceysanchez27@gmail.com>
2024-04-29 00:08:51 -0400

    intro to lists

---
+++ intro_lists/intro_lists.py
@@ -0,0 +1,7 @@
+# What is a List?
+
+# a list is one of many built-in data structures that allow us to work with 
+# a collection of data in sequential order 
+heights = [61, 70, 67, 64, 65]
+
+#________________________________________________________________________________
